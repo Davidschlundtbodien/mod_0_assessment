e689b046ded5b1835e6dcba66efaafb7f23317a7
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-04-23 12:54:26 -0600

    Add modelA

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var airport = {
+  name: "Mod 0 Airport",
+  location: "Turing",
+  numberOfGates: 20,
+  numberOfEmployees: 232,
+  isOpen: true,
+  hasHelicopters:false,
+  boardMembers: ["John", "Jim", "Jake"],
+  parkingGarages: ["North", "South", "East"]
+};
