186f19aacacaacb0d74c78a52ff1be78626dbb70
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-04-23 12:58:32 -0600

    Add modelB

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var vehicle = {
+  brand: "Honda",
+  model: "Civic",
+  year:2018,
+  milage: 3004,
+  isUsed: false,
+  hasSeatWarmer: true,
+  colors: ["Black", "Green", "Red"],
+  styles: ["Sport", "Sedan", "Hatchback"]
+};
