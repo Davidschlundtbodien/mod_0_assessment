65c224030b9c376ab4ccadf8f6d3bb2c882746e2
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-04-23 12:47:15 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,10 @@
+var famousPerson = "Will Smith";
+var isActive = true;
+famousPerson = "Tom Hanks";
+var num3 = 3;
+var num9 = 9;
+var sum = num3 + num9;
+var animals = ["Gorilla", "Lion", "Zebra"];
+
+animals.shift();
+famousPerson.length;
