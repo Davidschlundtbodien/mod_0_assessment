c39d53fd0e3a794a6df310b676967aae49ed0d07
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-04-27 13:10:09 -0600

    Change milage variable in modelB

--- models/modelB.js
+++ models/modelB.js
@@ -2,7 +2,7 @@ var vehicle = {
   brand: "Honda",
   model: "Civic",
   year:2018,
-  milage: 3004,
+  milageInMiles: 3004,
   isUsed: false,
   hasSeatWarmer: true,
   colors: ["Black", "Green", "Red"],
